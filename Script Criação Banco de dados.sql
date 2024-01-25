a82783f4b2a9371d4241b94a8b944023fe8434d4
Aldrik Alvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2024-01-25 19:19:09 -0300

    att banco

--- Script Criação Banco de dados.sql
+++ Script Criação Banco de dados.sql
@@ -1,4 +1,3 @@
-
 -- CRIANDO SCHEMA KARAOKE E USUARIO -- 
 DROP USER IF EXISTS 'user'@'localhost';
 DROP SCHEMA IF EXISTS karaoke;
@@ -8,8 +7,8 @@ CREATE USER 'user'@'localhost' IDENTIFIED BY 'karaoke123';
 GRANT SELECT, INSERT, DELETE, UPDATE ON karaoke.* TO 'user'@'localhost';
 
 -- CRIANDO TABELAS --
-CREATE TABLE musicas (
-    idMusica INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE musica (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(255),
     artista VARCHAR(255),
     genero VARCHAR(100),
@@ -17,7 +16,7 @@ CREATE TABLE musicas (
 );
 
 CREATE TABLE usuario (
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     senha VARCHAR(255),
@@ -27,7 +26,7 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE permissao (
-    idPermissao INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nomePermissao VARCHAR(50) UNIQUE
 );
 
@@ -35,32 +34,32 @@ CREATE TABLE usuarioPermissao (
     idUsuario INT,
     idPermissao INT,
     PRIMARY KEY (idUsuario, idPermissao),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-    FOREIGN KEY (idPermissao) REFERENCES permissao(idPermissao)
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+    FOREIGN KEY (idPermissao) REFERENCES permissao(id)
 );
 
 CREATE TABLE pedidos (
-    idPedido INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     idMusica INT,
     numeroMesa INT,
     idUsuario INT,
     dataPedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (idMusica) REFERENCES musicas(idMusica),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
+    FOREIGN KEY (idMusica) REFERENCES musica(id),
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
 );
 
 CREATE TABLE listasReproducao (
-    idLista INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nomeLista VARCHAR(255),
     idUsuario INT,
-    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
 );
 
-CREATE TABLE listasReproducaoMusicas (
+CREATE TABLE listasReproducaomusica (
     idLista INT,
     idMusica INT,
     ordem INT,
     PRIMARY KEY (idLista, idMusica),
-    FOREIGN KEY (idLista) REFERENCES listasReproducao(idLista),
-    FOREIGN KEY (idMusica) REFERENCES musicas(idMusica)
+    FOREIGN KEY (idLista) REFERENCES listasReproducao(id),
+    FOREIGN KEY (idMusica) REFERENCES musica(id)
 );
